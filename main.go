c96809a3c849dedec1a68685fd09a48b46e7187e
Cédric Anthony <Anthodev@users.noreply.github.com>
2024-02-26 18:22:25 +0100

    ✨ feat(symfony): added uuidgen + symfonySecretGen + copy to clipboard (#4)

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/hex"
 	"flag"
 	"fmt"
+	"github.com/atotto/clipboard"
+	"github.com/google/uuid"
 	"github.com/oklog/ulid"
 	"log"
 	"math/rand"
@@ -11,8 +13,8 @@ import (
 )
 
 func main() {
-	typeGen := flag.String("type", "ulid", "type of value to generate")
-	convertType := flag.String("convert", "", "type to convert to")
+	typeGen := flag.String("t", "ulid", "type of value to generate")
+	convertType := flag.String("c", "", "type to convert to")
 
 	flag.Parse()
 
@@ -27,13 +29,23 @@ func main() {
 		default:
 			res = generateUlid().String()
 		}
+	case "uuid":
+		res = generateUuid()
+	case "symfony":
+		res = generateRandomString()
 	}
 
 	fmt.Println(res)
+
+	copyToClipboard(res)
 }
 
 func generateUlid() ulid.ULID {
 	return ulid.MustNew(ulid.Timestamp(time.Now()), rand.New(rand.NewSource(time.Now().UnixNano())))
+}
+
+func generateUuid() string {
+	return uuid.NewString()
 }
 
 func convertUlidToUuidString(ulidVal ulid.ULID) string {
@@ -45,4 +57,30 @@ func convertUlidToUuidString(ulidVal ulid.ULID) string {
 
 	hexStr := hex.EncodeToString(ulidBytes)
 	return fmt.Sprintf("%s-%s-%s-%s-%s", hexStr[:8], hexStr[8:12], hexStr[12:16], hexStr[16:20], hexStr[20:])
+}
+
+func generateRandomString() string {
+	const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*"
+
+	bytes := make([]byte, 64)
+
+	_, err := rand.Read(bytes)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for i, b := range bytes {
+		bytes[i] = letters[b%byte(len(letters))]
+	}
+
+	return string(bytes)
+}
+
+func copyToClipboard(s string) {
+	if err := clipboard.WriteAll(s); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Copied to clipboard!")
 }
